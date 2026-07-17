12723be09d84529bcbdd9871b43a14aa5096cc56
FarrelSanjayaPutra <98817609+FarrelSanjayaPutra@users.noreply.github.com>
2026-07-18 00:32:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # Mengizinkan React (Frontend) mengakses API ini
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Di produksi, ganti dengan URL React Anda
+    allow_origins=["sampahinaja.vercel.app"],  # Di produksi, ganti dengan URL React Anda
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
